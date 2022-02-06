de743abe16c2656c2905741cc940019ffafcb4d4
arthurtomas <arthurtomas.ep@gmail.com>
2022-02-06 17:21:59 -0300

    Comentários adicionados

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Conta cc = new ContaCorrente();
-        cc.depositar(100);
-
-        Conta poupanca = new ContaPoupanca();
-
-        cc.transferir(100, poupanca);
-
-        cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
-        poupanca.simularRendimento(poupanca.getSaldo());
+        Conta cc = new ContaCorrente();        // Instanciando a Conta Corrente
+        cc.depositar(100);        // Depositando 100 reias na Conta Corrente
+        Conta poupanca = new ContaPoupanca();        // Instanciando a Conta Corrente
+        cc.imprimirExtrato();        // Imprimindo extrato da Conta Corrente antes da transferência
+        poupanca.imprimirExtrato();        // Imprimindo extrato da Poupança antes da transferência
+        cc.transferir(100, poupanca);        // Transferindo 100 reais da Conta Corrente para Poupança
+        cc.imprimirExtrato();        // Imprimindo extrato da Conta Corrente depois da transferência
+        poupanca.imprimirExtrato();        // Imprimindo extrato da Poupança depois da transferência
+        poupanca.simularRendimento(poupanca.getSaldo());        // Simulando rendimento da Poupança
     }
 }
